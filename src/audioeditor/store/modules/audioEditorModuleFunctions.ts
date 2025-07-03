a1c4d812d12a8d0f279d025400e0f154a0e6c0c5
YT <102475316+ytvee@users.noreply.github.com>
2025-07-03 17:37:07 +0300

    chore: remove unused audio editor module functions

--- src/audioeditor/store/modules/audioEditorModuleFunctions.ts
+++
@@ -1,6 +0,0 @@
-// import { AudioEditor } from "@/audioeditor/audiomodel/AudioEditor";
-// import { Context } from "./audioEditorModule";
-
-// export function setupAudioEditorListeners(context: Context, audioEditor: AudioEditor) {
-
-// }
