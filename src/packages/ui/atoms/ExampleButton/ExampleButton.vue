6a2377e2e7efb2e187c01691087668ac2d6986ac
YT <102475316+ytvee@users.noreply.github.com>
2025-07-12 19:46:59 +0300

    Add Storybook setup with ExampleButton component

---
+++ src/packages/ui/atoms/ExampleButton/ExampleButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="example-button" @click="$emit('click')">
+    <slot>Button</slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ExampleButton",
+};
+</script>
+
+<style scoped>
+.example-button {
+  padding: 0.5rem 1rem;
+  border: none;
+  background-color: #42b983;
+  color: white;
+  cursor: pointer;
+}
+</style>
